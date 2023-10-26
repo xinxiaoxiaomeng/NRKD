ecaa64f105757a3ba1de48852488076887c89333
xinxiaoxiaomeng <38664225+xinxiaoxiaomeng@users.noreply.github.com>
2023-10-26 12:38:12 +0800

    Update __init__.py

--- NRKD-master/helper/__init__.py
+++ NRKD-master/helper/__init__.py
@@ -1,7 +1,7 @@
 
 from .utils import *
 from .test import test_cls
-from .train import train, train_vanilla, train_metric_triplet, train_metric_kd
+from .train import train, train_vanilla
 import os,sys
 sys.path.append(os.getcwd())
 
